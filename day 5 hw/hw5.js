bea8b4ae5546d5d9049280d91213a87965cc5a4a
govinda472 <75463270+govinda472@users.noreply.github.com>
2024-06-17 23:59:10 -0700

    array

---
+++ day 5 hw/hw5.js
@@ -0,0 +1,19 @@
+//Homework: turn in a code sandbox
+//Write one example for every method
+
+
+
+// other useful ones
+// some, every, findIndex,
+
+// static:
+// isInteger, isNaN, parseInt, parseFloat
+
+// must know Strings
+// split, toUpperCase, toLowerCase
+// useful ones:
+// includes, charAt, slice, replace, substring, trim
+
+// must know Object
+// static
+// entries, keys, values
