644962a9f096724f0129163d75873ff8df620809
govinda472 <75463270+govinda472@users.noreply.github.com>
2024-06-18 00:59:01 -0700

    Update string.js

--- day 5 hw/string.js
+++ day 5 hw/string.js
@@ -1,14 +1,35 @@
-//Homework: turn in a code sandbox
-//Write one example for every method
+const firstname="govinda";
+const lastname="sahoo";
+const bio="I just graduated from ucsd with a cs degree";
 
+//split
+const words=bio.split(" ");
+console.log(words);
 
+//toUpperCase
+const upper=lastname.toUpperCase();
+console.log(upper);
 
+//toLowerCase
+const lower=upper.toLowerCase();
+console.log(lower);
 
+//includes
+console.log(bio.includes("ucsd"));
+console.log(bio.includes("cow"));
 
+//charAt
+console.log(firstname.charAt(0).toUpperCase());
 
+//slice
+console.log(bio.slice(0,26));
 
-// must know Strings
-// split, toUpperCase, toLowerCase
-// useful ones:
-// includes, charAt, slice, replace, substring, trim
+//replace
+console.log(bio.replace("ucsd","UC San Diego"));
 
+//substring
+console.log(bio.substring(0,6));
+
+//trim
+const white_spacebio=" I just graduated from ucsd  with a cs degree ";
+console.log(white_spacebio.trim());
