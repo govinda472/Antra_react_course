442f300358fcbcab36984ead93a33b40c1ecfe24
govinda472 <75463270+govinda472@users.noreply.github.com>
2024-06-18 00:38:12 -0700

    update

---
+++ day 5 hw/numbers.js
@@ -0,0 +1,16 @@
+// static:
+// isInteger, isNaN, parseInt, parseFloat
+// static:
+// isInteger, isNaN, 
+const div=5/2;
+console.log(Number.isInteger(div))
+console.log(Number.isInteger(1))
+console.log(Number.isInteger(1.25))
+console.log(Number.isInteger("123"))
+console.log(Number.isInteger("hey"))
+//isNan
+console.log(isNaN("s4"));
+//ParseInt
+console.log(parseInt('1234.99'));
+//ParseFloat
+console.log(parseFloat("1234.99"));
--- day 5 hw/static.js
+++
@@ -1,2 +0,0 @@
-// static:
-// isInteger, isNaN, parseInt, parseFloat
