3c62218e5bac1b1c6245ae4ac40fb519c4491f79
govinda472 <75463270+govinda472@users.noreply.github.com>
2024-06-20 09:54:58 -0700

    Update promise.js

--- day 7 hw/promise.js
+++ day 7 hw/promise.js
@@ -1,11 +1,23 @@
 export const fetchPosts = async () => {
   //fetch posts from "https://jsonplaceholder.typicode.com/posts"
   //return the posts
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(data.message || "Failed to fetch posts");
+  }
+  return data;
 };
 
 export const fetchPostById = async (id) => {
   //fetch a post by id from "https://jsonplaceholder.typicode.com/posts/${id}"
   //return the post
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(data.message || "Failed to fetch post");
+  }
+  return data;
 };
 
 export const sequentialPromise = async (promises, order) => {
@@ -20,4 +32,13 @@ export const sequentialPromise = async (promises, order) => {
   //   Promise.resolve("data3"),
   // ];
   //results = ["data2", "data1", "data3"]
+  const results = [];
+  for (let i=0; i<promises.length; i++) {
+    const result = await promises[order[i]];
+    if (result instanceof Error) {
+      throw result;
+    }
+    results.push(result);
+  }
+  return results;
 };
