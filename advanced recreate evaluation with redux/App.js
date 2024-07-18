2aa00fb31cfaae0cb571be442c698383e27947df
govinda472 <75463270+govinda472@users.noreply.github.com>
2024-07-17 21:21:48 -0700

    added html and style sheet

--- advanced recreate evaluation with redux/App.js
+++ advanced recreate evaluation with redux/App.js
@@ -1,10 +1,12 @@
 import react from 'react';
+import "./styles.css";
 import { connect, createStore } from 'react-redux';
 
+import rootReducer from './appReducer';
 
 export default function App() {
-    
 
+    const store = createStore(rootReducer);
 
 
 
@@ -15,7 +17,29 @@ export default function App() {
 
     return (
         <div>
-            <h1>App</h1>
-        </div>
+<div class="layout">
+      
+      
+    
+      <div class="box" id="available_courses">
+          <h3>avaliable courses</h3>
+      </div>
+      
+      <div class="box" id="selected_courses">
+          <h3>selected courses</h3>
+      </div>
+      
+      
+      
+      </div>
+
+      <footer>
+      <span>
+      <p>total credits:<span id="total_credits">0</span></p>
+      <button id="button">select</button>
+      </span>
+      </footer>
+      </div>
     )
 }
+
--- advanced recreate evaluation with redux/appReducer.js
+++ advanced recreate evaluation with redux/appReducer.js
@@ -1,4 +1,4 @@
-import { createStore, connect } from 'react-redux'
+import { combineReducers,createStore, connect } from 'react-redux'
 const data = [{
     "id":1,
     "courseName":"React",
@@ -31,11 +31,39 @@ const data = [{
 }
 ];
 
+const availableCourses = [];
 
-const availableCoursesReducer =(state=data, action)=>{
+
+const availableCoursesReducer = (state=availableCourses, action)=>{
     switch(action.type){
         case 'SET_COURSES':
-            return action.payload
+            for(let i=0; i<action.payload.length; i++){
+                state.push(action.payload[i]);
+            }
+            return state;
+        case 'ADD_COURSE':
+            state.push(action.payload);
+            return state;
+        case 'REMOVE_COURSE':
+            const index = state.findIndex(course=>course.id === action.payload.id);
+            state.splice(index, 1);
+            return state;
+        default:
+            return state
+    }
+}
+
+const selectedCourses = [];
+
+const selectedCoursesReducer = (state=selectedCourses, action)=>{
+    switch(action.type){
+        case 'ADD_SELECTED_COURSE':
+            state.push(action.payload);
+            return state;
+        case 'REMOVE_SELECTED_COURSE':
+            const index = state.findIndex(course=>course.id === action.payload.id);
+            state.splice(index, 1);
+            return state;
         default:
             return state
     }
@@ -43,9 +71,9 @@ const availableCoursesReducer =(state=data, action)=>{
 
 
 
+const rootReducer = combineReducers({
+    available: availableCoursesReducer,
+    selected: selectedCoursesReducer
+  });
 
-
-
-export default function appReducer() {
-    
-}
+export default rootReducer;
